a7376ace8f62d14148c0144e8e8ae18a22b3029c
Cyprian <bieglywybuch@gmail.com>
2025-11-09 20:24:18 +0100

    Implemented funcions for: deleting; students, grades and subjects

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 students = {"Cyprian" : 
-            {"Matematyka" : [],
-             "Polski ": []},
+            {"Matematyka" : [5, 4],
+             "Polski": []},
             "Lena" : 
-            {"Matematyka " : []}
+            {"Matematyka" : []}
             }
 
 
@@ -15,21 +15,57 @@ def add_student(student):
 def add_subject(student, subject):
     if student not in students:
         print("This student doesn't exist")
-    elif subject not in students[student]:
-        students[student][subject] = []
+        return None
+    if subject in students[student]:
+        print("This subject already exists")
+        return None
+    students[student][subject] = []
 
 def add_grade(student, subject, grade):
     if student not in students:
         print("Student doesn't exist ")
+        return None
     elif subject not in students[student]:
         print("Subject doesn't exist")
+        return None
     students[student][subject].append(grade)
-    
 
+def print_grades(student, subject):
+    if student not in students:
+        return None
+    elif subject not in students[student]:
+        return None 
+    elif not students[student][subject]:
+        print(f"{student} doesn't have any grades.")
+        return None
+    print(f"{student} have {students[student][subject]} in {subject}")
 
+def main():
+    while True: 
+        ...
 
+def delete_student(student):
+    if student not in students:
+        print("Student not found")
+    else:
+        del students[student]
 
+def delete_latest_grade(student, subject):
+    if student not in students:
+        return None
+    elif subject not in students[student]:
+        return None
+    elif not students[student][subject]:
+        print("Student doesn't have any grades")
+        return None
+    removed_grade = students[student][subject].pop()
+    print(f"Removed {removed_grade} from {student}'s {subject}")
 
-        
-
+def delete_subject(student, subject):
+    if student not in students:
+        return None 
+    elif subject not in students[student]:
+        return None
+    removed_subject = students[student].pop(subject)
+    print(f"Deleted {subject} from {student}'s profile")
 
